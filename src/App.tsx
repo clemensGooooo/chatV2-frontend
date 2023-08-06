e0223f88d0749acc1c034e65a0aa7ebaab4e4abe
Clemens <100614200+clemensGooooo@users.noreply.github.com>
2023-08-06 17:21:35 +0200

    geändert:       src/env.ts 	gelöscht:       src/pages/Dashboard.tsx 	gelöscht:       src/pages/Dashboard/Account.tsx 	gelöscht:       src/pages/Dashboard/ShowNews.tsx 	gelöscht:       src/pages/Dashboard/Users.tsx 	gelöscht:       src/pages/Dashboard/WriteNews.tsx 	geändert:       src/pages/Login.tsx 	geändert:       src/pages/Registration.tsx 	geändert:       src/pages/Welcome.tsx 	gelöscht:       src/pages/error/404.tsx 	geändert:       src/providers/ConnectionChecker.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/Dashboard';
-import { Account } from './pages/Dashboard/Account';
-import { ShowNews } from './pages/Dashboard/ShowNews';
-import { Users } from './pages/Dashboard/Users';
-import { WriteNews } from './pages/Dashboard/WriteNews';
+import { Account } from './pages/Dashboard/User/Account';
+import { ShowNews } from './pages/Dashboard/User/ShowNews';
+import { Users } from './pages/Dashboard/Admin/Users';
+import { WriteNews } from './pages/Dashboard/Admin/WriteNews';
 import { Error404 } from './pages/error/404';
 import Login from './pages/Login';
 import { Register } from './pages/Registration';
