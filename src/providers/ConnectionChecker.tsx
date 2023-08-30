a9e23183d62011e13898de0a1aef7f0e718e0bfb
Clemens <clemens.koehler@mail.de>
2023-08-30 08:48:50 +0200

    geändert:       src/env.ts 	geändert:       src/providers/ConnectionChecker.tsx 	geändert:       package-lock.json 	geändert:       package.json 	geändert:       src/App.tsx 	geändert:       src/env.ts 	geändert:       src/pages/Dashboard/User/Account.tsx 	src/pages/Chat/

--- src/providers/ConnectionChecker.tsx
+++ src/providers/ConnectionChecker.tsx
@@ -1,7 +1,7 @@
-import { Box, CircularProgress } from "@mui/material";
+import { Alert, AlertTitle, Box, CircularProgress } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { url_main } from "../env";
+import { headers, urls } from "../env";
 import axios from "axios";
 
 const styleLoading: React.CSSProperties = {
@@ -13,27 +13,43 @@ const styleLoading: React.CSSProperties = {
     margin: 'auto'
 }
 
+const styleServerOff: React.CSSProperties = {
+    display: 'flex',
+    justifyContent: "center",
+    alignItems: "center",
+    height: '100vh'
+}
 export const ConnectionChecker = (props: { children: React.ReactNode }) => {
     const location = useLocation();
-
     const [loading, setLoading] = useState(true);
-    const token = localStorage.getItem('token');
+    const [serverOff, setServerOff] = useState(false)
+
+
     useEffect(() => {
         checkConnection();
     }, [location]);
 
     const checkConnection = async () => {
         try {
-            const headers = {
-                Authorization: 'Bearer ' + token,
-            };
-            await axios.get(url_main + 'check_connection', { headers });
+            await axios.get(urls.connection_check, { headers });
             setLoading(false)
-            
         } catch (err) {
             localStorage.removeItem('token');
-            if (location.pathname === "/login" ||location.pathname === "/register") {
+            if (location.pathname === "/login" || location.pathname === "/register") {
                 setLoading(false);
+                // Check serveravailibility
+                try {
+                    const response = await axios.get(urls.connection_check);
+                } catch (error) {
+                    console.log(error);
+
+                    if (axios.isAxiosError(error)) {
+                        if (error.message == "Network Error") {
+                            setServerOff(true);
+                        }
+                    }
+                }
+
             } else {
                 window.location.href = "/login";
             }
@@ -47,7 +63,16 @@ export const ConnectionChecker = (props: { children: React.ReactNode }) => {
                         style={styleLoading} />
                 </Box>
                 : <>
-                    {props.children}
+                    {serverOff ?
+                        <>
+                            <Box sx={styleServerOff}>
+                                <Alert severity="error">
+                                    <AlertTitle>Network - Error</AlertTitle>
+                                    The server is not reachable - <strong>Please try to connect later again !</strong>
+                                </Alert>
+                            </Box>
+                        </>
+                        : <>{props.children}</>}
                 </>
             }
         </div >
