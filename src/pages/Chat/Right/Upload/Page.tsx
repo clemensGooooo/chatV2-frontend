04b2a6a2bf388d75508627eb7ef0bfc6caefb6ea
Clemens <100614200+clemensGooooo@users.noreply.github.com>
2023-09-19 18:16:23 +0200

    geändert:       src/pages/Chat/Right/Index.tsx 	geändert:       src/pages/Chat/Right/Send/Send.tsx 	geändert:       src/pages/Chat/Right/Upload/Page.tsx

--- src/pages/Chat/Right/Upload/Page.tsx
+++ src/pages/Chat/Right/Upload/Page.tsx
@@ -12,81 +12,51 @@ import { useEffect, useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 
 interface PageProps {
-  back: () => void;
   change: () => void;
   uploadedFile: File | null;
 }
 
-const Page: React.FC<PageProps> = ({
-  back,
-  change: majorChange,
-  uploadedFile,
-}) => {
+const Page: React.FC<PageProps> = ({ change: majorChange, uploadedFile }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     setIsOpen(true);
   }, []);
 
-  const handleBack = () => {
-    setIsOpen(false);
-    setTimeout(() => {
-      back();
-    }, 500);
-  };
-
   const theme = useTheme();
-
-  const styles = {
-    paper: {
-      zindex: 1,
-      overflow: "scroll",
-      width: "100%",
-      position: "absolute",
-      display: "block",
-      borderBottomRightRadius: "30px",
-      borderTopRightRadius: "30px",
-      textAlign: "left",
-      height: "100%",
-      left: isOpen ? "0%" : "100%",
-      transition: "left 0.5s ease-in-out",
-      boxShadow: "none",
-      borderTopLeftRadius: "0px",
-      backgroundColor: theme.palette.background.paper,
-    },
-    body: {
-      padding: "10px",
-    },
-    closeButton: {
-      display: "block",
-      position: "absolute",
-      right: "10px",
-    },
-  };
 
   if (uploadedFile == null) return <></>;
   return (
-    <Paper elevation={2} sx={styles.paper}>
-      <AppBar
-        position="static"
+    <Paper
+      elevation={2}
+      style={{
+        flex: 1,
+        position: "relative",
+        height: "100%"
+      }}
+    >
+      <Box
         sx={{
-          borderTopRightRadius: "30px",
-          boxShadow: "none",
+          padding: "10px",
+          width: "100%",
+          maxHeight: "100%",
+          overflowY: "auto",
         }}
-        color={"secondary"}
       >
-        <Toolbar>
-          <Typography variant="h5">Send a image</Typography>
-          <IconButton
-            aria-label="close"
-            sx={styles.closeButton}
-            onClick={handleBack}
-          >
-            <CloseIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Box>
+        <img
+          style={{
+            width: "100%",
+          }}
+          src={URL.createObjectURL(uploadedFile)}
+          alt="Uploaded"
+        />{" "}
+        <img
+          style={{
+            width: "100%",
+          }}
+          src={URL.createObjectURL(uploadedFile)}
+          alt="Uploaded"
+        />{" "}
         <img
           style={{
             width: "100%",
